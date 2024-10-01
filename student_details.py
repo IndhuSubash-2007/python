ef377f011977b7c72bc5a6381945b240c2bfa4d4
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-10-01 15:36:19 +0530

    Document Header

--- student_details.py
+++ student_details.py
@@ -1,3 +1,9 @@
+'''
+Python program to get student details
+Author: Indhu Subash
+Date: 01-10-2024
+Version:1.0
+ '''
 name=input("Enter the student name:")
 roll_number=int(input("Enter your roll number:"))
 roll_number=roll_number+1
