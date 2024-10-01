28a9750e5b15c01ad54ce42a55f8817e7dde7250
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-10-01 14:57:02 +0530

    Add files via upload

---
+++ student_details.py
@@ -0,0 +1,8 @@
+name=input("Enter the student name:")
+roll_number=int(input("Enter your roll number:"))
+roll_number=roll_number+1
+cgpa=float(input("Enter the CGPA:"))
+percentage_mark = cgpa*10
+print("Name of the student:",name)
+print("Roll Number:",roll_number)
+print("Marks Percentage:",percentage_mark,"%")
