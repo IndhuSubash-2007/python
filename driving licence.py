c7236a278e5eb01e9eaf934928fe2ce3a2886645
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-10-15 14:47:36 +0530

    Add files via upload

---
+++ driving licence.py
@@ -0,0 +1,10 @@
+'''
+Author:Indhu Subash
+Date:15-10-2024
+'''
+
+age=int(input("Enter your age:"))
+if(age>=16):
+    print("Eligible for driving licence")
+else:
+    print("Not eligible for driving licence")
