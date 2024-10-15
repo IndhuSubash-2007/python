2d41ce2d38fe326ef742850b43940b7bbd9771e8
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-10-15 15:03:24 +0530

    Add files via upload

---
+++ FARE.py
@@ -0,0 +1,12 @@
+'''
+Author:Indhu Subash
+Date:15-10-2024
+'''
+
+age=int(input("Enter your age:"))
+if(age<10):
+    print("Fare is 7")
+elif(age>=10 and age<=60):
+    print("Fare is 10")
+else:
+    print("Fare is 5")
