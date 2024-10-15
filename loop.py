56631bf1403ac8d4f393c2d1b180752682ba2dd0
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-10-15 15:58:20 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,12 @@
+'''
+Author:Indhu Subash
+Date:15-10-2024
+'''
+
+limit=int(input("Enter the limit:"))
+odd_number=1
+count=0
+while count<limit:
+    print(odd_number,"\t",end="")
+    count+=1
+    odd_number+=2
