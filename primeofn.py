7ba801a428806bdb1905b5830c874c65b5ba55b6
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-10-29 14:53:21 +0530

    Add files via upload

---
+++ primeofn.py
@@ -0,0 +1,15 @@
+'''
+AUTHOR:INDHU SUBASH
+DATE:29-10-24
+PROGRAM:FOR LOOP
+'''
+
+limit=int(input("Enter the limit:"))
+for number in range(2,limit+1):
+    isPrime = True
+    for i in range(2,number//2+1):
+        if number%i==0:
+            isPrime=False
+            break
+    if isPrime:
+     print(number,end=" ")
